832701baa812a7e2f2dacc417607ef2f0f24bfc8
ebkn <ktennis.mqekr12@gmail.com>
2018-05-03 00:39:01 +0900

    feat: introduce react

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,22 +1,29 @@
 // @flow
 
 import path from 'path'
+import webpack from 'webpack'
 
 import { WDS_PORT } from './src/shared/config'
 import { isProd } from './src/shared/util'
 
 export default {
+  mode: isProd ? 'production' : 'development',
   entry: [
+    'react-hot-loader/patch',
     './src/client',
   ],
   output: {
-    filename: 'js/bndle.js',
+    filename: 'js/bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: isProd ? '/static/' : `http://localhost:${WDS_PORT}/dist/`,
   },
   module: {
     rules: [
-      { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
     ],
   },
   devtool: isProd ? false : 'source-map',
@@ -25,6 +32,16 @@ export default {
   },
   devServer: {
     port: WDS_PORT,
+    hot: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
   },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
 }
 
