16adb22d62ebc7e772363cfa2b3cff7ca8591d0f
ebkn <ktennis.mqekr12@gmail.com>
2018-05-03 23:23:02 +0900

    feat: introduce bootstrap

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -17,6 +17,13 @@ import { APP_CONTAINER_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 import setUpSocket from './socket'
 
+import $ from 'jquery'
+import Tether from 'tether'
+
+window.jQuery = $
+window.Tether = Tether
+require('bootstrap')
+
 /* eslint-disable-next-line no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 const preloadedState = window.__PRELOADED_STATE__
